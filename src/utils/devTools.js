3e911637035f216bffda4b621ac7e100b26b86e8
Eugene Traytyak <evgen.traytyak@gmail.com>
2016-01-10 22:29:26 +0300

    Update devtools util

--- src/utils/devTools.js
+++ src/utils/devTools.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import { createStore as initialCreateStore, compose } from 'redux';
+import { createDevTools } from 'redux-devtools';
+import LogMonitor from 'redux-devtools-log-monitor';
+import DockMonitor from 'redux-devtools-dock-monitor';
 
-export let createStore = initialCreateStore;
-
-if (__DEV__) {
-    createStore = compose(
-        require('redux-devtools').devTools(),
-        require('redux-devtools').persistState(
-            window.location.href.match(/[?&]debug_session=([^&]+)\b/)
-        ),
-        createStore
-    );
-}
-
-export function createDevTools(store) {
-    if (__DEV__) {
-        let { DevTools, DebugPanel, LogMonitor } = require('react-devtools/lib/react');
-        return (
-            <DebugPanel top right bottom>
-                <DevTools store={store} monitor={LogMonitor} />
-            </DebugPanel>
-        );
-    }
-
-    return null;
-}
+export default createDevTools(
+    <DockMonitor toggleVisibilityKey='ctrl-h'
+                 changePositionKey='ctrl-q'>
+        <LogMonitor />
+    </DockMonitor>
+);
