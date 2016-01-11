41c07264f76ce01413793b89607d6066fef893b4
Eugene Traytyak <evgen.traytyak@gmail.com>
2016-01-11 11:57:23 +0300

    Add FriendListApp component

---
+++ src/containers/FriendListApp.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class FriendListApp extends Component {
+    render() {
+        return (
+            <div>Hello world!</div>
+        );
+    }
+}
