4314e957b5fcc548849fea87a509de62c9032278
Eugene Traytyak <evgen.traytyak@gmail.com>
2016-01-13 12:03:29 +0300

    Add propTypes to FriendListItem component

--- src/components/FriendListItem.js
+++ src/components/FriendListItem.js
@@ -1,6 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class FriendListItem extends Component {
+    static propTypes = {
+        name: PropTypes.string.isRequired,
+        id: PropTypes.number.isRequired
+    }
+
     render() {
         return (
             <li>
