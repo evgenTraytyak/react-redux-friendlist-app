5bfe0fb66e50bcf591e85d690e77aada211a9ba2
Eugene Traytyak <evgen.traytyak@gmail.com>
2016-01-13 11:33:15 +0300

    Update FriendListApp. Inject props into stupid component

--- src/containers/FriendListApp.js
+++ src/containers/FriendListApp.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import * as FriendsActions from '../actions/FriendsActions';
+import FriendList from '../components/FriendList';
+
+@connect(state => ({
+  friendlist: state.friendlist
+}))
 
 export default class FriendListApp extends Component {
     render() {
+        const { friendlist: { friendsById }, dispatch } = this.props;
+        const actions = bindActionCreators(FriendsActions, dispatch);
+
         return (
-            <div>Hello world!</div>
+            <div>
+                <FriendList friends={friendsById} actions={actions} />
+            </div>
         );
     }
 }
